8b1ab97f577739ef6feed5092101557176f80012
Sybren <sybren@sybrial.com>
2019-04-28 15:34:14 +0200

    Add project files.

---
+++ LatticeCraft.GameServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LatticeCraft.GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
